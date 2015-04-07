78d929735597ad28954fa50af48ae9bec4871fa5
zoobab <zoobab@gmail.com>
2015-04-07 15:00:38 +0200

    openwrt-add_ip.sh: fix for the network add

--- etc/vz/dists/scripts/openwrt-add_ip.sh
+++ etc/vz/dists/scripts/openwrt-add_ip.sh
@@ -8,7 +8,7 @@ uci set network.${VENET_DEV}.proto=static
 uci set network.${VENET_DEV}.ipaddr=${IP_ADDR}
 uci set network.${VENET_DEV}.netmask=255.255.255.0
 
-uci get network.@route[0] > /dev/null 2>&1 || echo -ne "Adding network route: " && sleep 1 && uci add network route
+uci get network.@route[0] > /dev/null 2>&1 || (echo -ne "Adding network route: " && sleep 1 && uci add network route)
 uci set network.@route[0]=route
 uci set network.@route[0].interface=${VENET_DEV}
 uci set network.@route[0].target=0.0.0.0
